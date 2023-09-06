437ed7feec72424f6f34ae8c1db5a35ab3ec49ed
Fritz John S. Saycon <61639958+Fritz-John@users.noreply.github.com>
2023-09-06 16:11:37 +0800

    Update PlayerMoveCamera.cs

--- Assets/Scripts/PlayerMoveCamera.cs
+++ Assets/Scripts/PlayerMoveCamera.cs
@@ -92,7 +92,7 @@ public class PlayerMoveCamera : NetworkBehaviour
         cameraRay = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
         PlayerMouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        float sphereCastRadius = 0.1f;
+        float sphereCastRadius = 0.15f;
         isGrounded = Physics.SphereCast(transform.position, sphereCastRadius, Vector3.down, out hit, 0.9f);
 
         
@@ -295,7 +295,6 @@ public class PlayerMoveCamera : NetworkBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-
     }
 
     void MovePlayerCamera()
