55334a4dc9197724795aa2f27f52ae407199f66d
Fritz John S. Saycon <61639958+Fritz-John@users.noreply.github.com>
2023-09-24 12:36:53 +0800

    updates

--- Assets/Scripts/CameraShaker.cs
+++ Assets/Scripts/CameraShaker.cs
@@ -6,6 +6,9 @@ public class CameraShaker : NetworkBehaviour
 {
     public float shakeDuration = 0.5f;
     public float shakeMagnitude = 0.2f;
+    public Transform tpCam;
+    public GameObject cube;
+    private Vector3 cubePos;
 
     [SyncVar]
     private bool isShaking = false;
@@ -13,13 +16,28 @@ public class CameraShaker : NetworkBehaviour
     private float shakeTimer = 0f;
     private GameObject currentPlayerInZone;
 
+    private void Start()
+    {
+        cubePos = cube.transform.position;
+    }
+
     private void OnTriggerStay(Collider other)
     {
         GameObject player = other.gameObject;
 
         if (player.CompareTag("Player"))
-        {    
+        {
+            Vector3 directionToPlayer = player.transform.position - cube.transform.position;
+            float distanceToPlayer = directionToPlayer.magnitude;
+            cube.GetComponent<MeshRenderer>().enabled = true;
+            
             isShaking = true;
+            if (distanceToPlayer > 2f) 
+            {
+                Vector3 moveDirection = directionToPlayer.normalized;
+                cube.transform.position += moveDirection * 2 * Time.deltaTime;
+            }
+            
             CmdStartShake(player);
         }
     }
@@ -30,6 +48,8 @@ public class CameraShaker : NetworkBehaviour
 
         if (player.CompareTag("Player"))
         {
+            cube.GetComponent<MeshRenderer>().enabled = false;
+            cube.transform.position = cubePos;
             CmdStopShake();
         }
     }
@@ -61,13 +81,16 @@ public class CameraShaker : NetworkBehaviour
 
     private IEnumerator Shake(Camera camera)
     {
+        camera.transform.position = tpCam.transform.position;
         while (isShaking)
         {
+
+          
             Vector3 originalPosition = camera.transform.localPosition;
             Vector3 randomOffset = Random.insideUnitSphere * shakeMagnitude;
 
             camera.transform.localPosition = originalPosition + randomOffset;
-
+           
             shakeTimer -= Time.deltaTime;
 
             yield return null;
